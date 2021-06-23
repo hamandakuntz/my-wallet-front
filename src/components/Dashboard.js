924adf72b0aff6ea829b1feee73d4215448206b7
hamandakuntz <hamanda.monteiro@hotmail.com>
2021-06-22 23:15:47 -0300

    tela de transacoes implementada (layout e map com array ficticia)

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,11 +2,61 @@ import { useEffect, useState, useContext } from "react";
 import styled from "styled-components";
 import axios from "axios";
 import UserContext from "../contexts/UserContext";
+import { IoLogOutOutline } from "react-icons/io5";
+import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
+import Transaction from "./Transaction";
 
 export default function Dashboard() {
-    const [transactions, setTransactions] = useState([]);
+    const [transactions, setTransactions] = useState([
+        {
+            date: "30/11",
+            description: "Almoço mãe",
+            value: 3990,
+            type: "output",
+        },
+        {
+            date: "27/11",
+            description: "Mercado",
+            value: 54254,
+            type: "output",
+        },
+        {
+            date: "26/11",
+            description: "Compras churrasco",
+            value: 6760,
+            type: "output",
+        },
+        {
+            date: "20/11",
+            description: "Empréstimo Maria",
+            value: 50000,
+            type: "entry",
+        },
+        {
+            date: "15/11",
+            description: "Salário",
+            value: 300000,
+            type: "entry",
+        },
+    ]);    
+      
     const { userData } = useContext(UserContext);
     const localUser = JSON.parse(localStorage.getItem("user"));
+    const [name, setName] = useState("");
+
+    let total = 0;
+   
+    transactions.forEach(item => {
+        if(item.type === "entry") {
+            total += item.value;
+        } else {
+            total -= item.value;
+        }
+    });
+
+    // useEffect(() => {
+    //     getTransactions()      
+    // }, []);
 
     function getTransactions() {
         const config = {
@@ -18,7 +68,8 @@ export default function Dashboard() {
             config
           );
           request.then((response) => {
-            setTransactions(response.data.transactions);            
+            setTransactions(response.data.transactions); 
+            setName(response.data.user.name);           
           });
           request.catch(() => {
             alert("Houve uma falha ao obter suas transações, por favor, atualize a página.");
@@ -27,19 +78,119 @@ export default function Dashboard() {
 
     return (
        <Container>
-           oi
+           <Header>
+            <Title>{`Olá ${name}`}</Title>
+            <IoLogOutOutline className="logout-icon"/>
+           </Header>  
+           <TransactionsRegisterCard transactions={transactions}>
+               {transactions.length === 0 ? <div>Não há registros de entrada ou saída</div> :                
+               transactions.map((t, i) => (
+                <Transaction                  
+                  item={t}
+                  key={i}
+                />               
+               ))}
+            {transactions.length === 0 ? "" : 
+                <Total total={total}>
+                    <h1>SALDO</h1>
+                    <span>{(total/100).toFixed(2).replace(".",",").replace("-","")}</span>
+                </Total>
+            }                  
+           </TransactionsRegisterCard>     
+           <Wrapper>  
+                <NewTransationCard>  
+                    <AiOutlinePlusCircle className="plus-icon" />
+                   <h1>Nova</h1>
+                   <h2>entrada</h2>
+                </NewTransationCard>
+                <NewTransationCard> 
+                    <AiOutlineMinusCircle className="minus-icon" />                   
+                    <h1>Nova</h1>
+                    <h2>saída</h2>
+                </NewTransationCard>
+           </Wrapper>  
        </Container>
     )
 }
 
 const Container = styled.div`
+  font-family: "Raleway";
   background: #8c21be;
   height: 100vh;
-
-  img {
-    margin-top: 150px;
-    margin-bottom: 35px;
-    margin-left: 113px;
-  }
+  padding: 20px;  
 `;
 
+const Header = styled.div`
+    display:flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .logout-icon {        
+        font-size: 30px;       
+        color: white;       
+    }
+`;
+
+const Title = styled.div`  
+  font-weight: 700;    
+  font-size: 26px;  
+  color: white;
+`;
+
+const TransactionsRegisterCard = styled.div`
+    height: 446px;    
+    border-radius: 5px;
+    background: #fff;
+    margin-top: 10px;  
+    padding: 10px;    
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 15px;
+`;
+
+const NewTransationCard = styled.div`
+    height: 114px;
+    width: 155px;    
+    border-radius: 5px;
+    background-color: #A328D6;
+    display: flex;
+    flex-direction: column;
+    
+    .plus-icon, .minus-icon {
+        color: white;
+        font-size: 25px;
+        margin: 10px;
+        margin-bottom: 25px;
+    }
+
+    h1, h2 {        
+        font-size: 17px;        
+        font-weight: 700;
+        line-height: 20px;
+        letter-spacing: 0em;        
+        color: white;
+        padding-left: 10px;
+    }
+`;
+
+const Total = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    position: absolute;    
+    bottom: 170px;
+    padding-right: 60px;
+
+    h1 {
+        font-size: 17px;
+        color: #000000;
+        font-weight: 700;
+    }
+
+    span {
+        color: ${props => props.total < 0 ? "#C70000" : "#50AD0E"}
+    }
+`;
---
+++ src/components/Transaction.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export default function Transaction(props) {
+    const { date, description, value, type } = props.item;
+
+    return (
+        <Container type={type}>
+            <Wrapper>
+                <Date>{date}</Date>
+                <Description>{description}</Description>
+            </Wrapper> 
+            <span className="value">{(value/100).toFixed(2).replace(".",",")}</span>
+        </Container>       
+    );
+}
+
+const Container = styled.div`
+    width: 100%;  
+    display: flex;
+    justify-content: space-between;
+    font-size: 16px;
+    padding-top: 20px;    
+    align-items: center;
+
+    .value {
+        color: ${props => props.type === "output" ? "#C70000" : "#03AC00"};        
+    }
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    align-items: flex-end;      
+`;
+
+const Date = styled.div`
+    padding-right: 10px;
+    padding-bottom: 2px;
+    color: #C6C6C6;
+`;
+
+
+const Description = styled.div`  
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 19px;
+    letter-spacing: 0em;
+    text-align: left;
+    color: black;
+`;
